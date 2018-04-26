08b53bc193c98437d6dabb6ff9bce3e8bc192b2c
wasamiam <wasamiam@gmail.com>
2018-04-26 13:15:41 -0700

    Update

--- wilsosam - homework1.hs
+++ wilsosam - homework1.hs
@@ -24,9 +24,10 @@ y2 = ""
 vector = Def "vector" [x1, y1, x2, y2] (Comb (Comb (Comb (Pen Up) (Moveto (S x1) (S y1) ) ) (Pen Down) ) (Comb (Moveto (S x2) (S y2) ) (Pen Up) ) )
 
 -- 1(c):
--- steps :: Int -> Cmd
--- steps 0 = (Pen Up)
--- steps n = Comb() (steps (n-1) )
+-- steps = pen up; moveto (n, n); pen down; moveto (n-1, n); moveto (n-1, n-1); steps (n-1)
+steps :: Int -> Cmd
+steps 0 = (Pen Up)
+steps n = Comb (Comb (Comb (Comb (Pen Up) (Moveto (I n) (I n) ) ) (Comb (Pen Down) (Moveto (I (n-1) ) (I n) ) ) ) (Moveto (I (n-1) ) (I (n-1) ) ) ) (steps (n-1) )
 
 -- 2(a):
 data Circut = Cir Gates Links
@@ -38,7 +39,13 @@ type Links = [(Int, Int, Int, Int)]
 circut2b = Cir [(1, Xor),(2, And)] [(1,1,2,1),(1,2,2,2)]
 
 -- 2(c):
+
 -- 3(a):
+data Expr = N Int
+          | Plus Expr Expr
+          | Times Expr Expr
+          | Neg Expr
+
 data Op = Add | Multiply | Negate
 data Exp = Num Int
          | Apply Op [Exp]
@@ -49,3 +56,8 @@ exp3a = Apply Negate [ Apply Multiply [ Apply Add [ Num 3, Num 4 ], Num 7 ] ]
 -- The alternate form seems more readable for me and it allows for preforming operations on more than two Nums at a time.
 
 -- 3(c):
+translate :: Expr -> Exp
+translate (N n) = (Num n)
+translate (Plus ex1 ex2) = (Apply Add [translate ex1, translate ex1])
+translate (Times ex1 ex2) = (Apply Multiply [translate ex1, translate ex2])
+translate (Neg ex) = (Apply Negate [translate ex])
